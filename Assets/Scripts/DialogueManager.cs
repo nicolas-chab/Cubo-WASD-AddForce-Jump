516bdc91413fcf0f7f0c86d3b177db9ccbcf9541
nicolas-chab <101563616+nicolas-chab@users.noreply.github.com>
2022-08-29 12:12:53 -0300

    finished

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField] GameObject dialogueUI;
+    [SerializeField]TextMeshProUGUI textodeldialogo;
+    [SerializeField] string[] frasesDialogo;
+    [SerializeField] int posicionfrase;
+    [SerializeField] TextMeshProUGUI textoboton;
+    [SerializeField] bool hastalked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+        //al entrar activa la ui del dialogo
+        if (other.gameObject.CompareTag("npc"))
+        {
+            frasesDialogo = other.gameObject.GetComponent<NPCBEHAVIOUR>().data.dialogueFrases;
+            if (!hastalked)
+            {
+                
+                textodeldialogo.text = "Hola forastero";
+                dialogueUI.SetActive(true);
+            }
+            else
+            {
+                textodeldialogo.text = "Ya hemos hablado";
+                dialogueUI.SetActive(true);
+                textoboton.text = "cerrar";
+            }
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        //al salir desaparece el dialogo
+        if (other.gameObject.CompareTag("npc"))
+        {
+            dialogueUI.SetActive(false);
+        }
+    }
+    public void nextphrase()
+    {
+        if (posicionfrase < frasesDialogo.Length)
+        {
+
+            textodeldialogo.text = frasesDialogo[0];
+            posicionfrase++;
+            if (posicionfrase==frasesDialogo.Length)
+            {
+                textoboton.text = "cerrar";
+                
+            }
+        }
+        else
+        {
+            dialogueUI.SetActive(false);
+            hastalked=true;
+        }
+        
+    }
+}
